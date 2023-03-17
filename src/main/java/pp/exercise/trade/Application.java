94a75364432f421bd833f2b4ec0f3eac847c75a0
Pawel <pawel.piotrowski94@gmail.com>
2023-03-17 17:11:14 +0100

    Changed application logic to read and execute methods specified in the instruction file. Assuming that the set of methods in the Algo library is immutable, now the only thing needed to add a new signal is to define appropriate steps for its handling in the procedures.yaml file. Also cleaned up the main TradeApplication.java file after previous tests.

--- src/main/java/pp/exercise/trade/Application.java
+++ src/main/java/pp/exercise/trade/Application.java
@@ -1,38 +1,74 @@
 package pp.exercise.trade;
 
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 import pp.exercise.trade.library.Algo;
 import pp.exercise.trade.library.SignalHandler;
+import pp.exercise.trade.model.Instruction;
+import pp.exercise.trade.model.Procedure;
+import pp.exercise.trade.model.Procedures;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Application implements SignalHandler {
+    private Procedures proceduresDefinitions;
+    private final Algo algo = new Algo();
+
+    public Application() {
+        try {
+            InputStream inputStream = new FileInputStream("src/main/resources/procedures.yaml");
+            Yaml yaml = new Yaml(new Constructor(Procedures.class));
+            proceduresDefinitions = yaml.load(inputStream);
+        }
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            proceduresDefinitions = null;
+        }
+    }
+
     public void handleSignal(int signal) {
-        Algo algo = new Algo();
+        Procedure procedure = proceduresDefinitions.getProcedures().stream()
+                .filter(proc -> proc.getSignal().equals(signal))
+                .findFirst()
+                .orElse(null);
 
-        switch (signal) {
-            case 1:
-                algo.setUp();
-                algo.setAlgoParam(1, 60);
-                algo.performCalc();
-                algo.submitToMarket();
-                break;
-
-            case 2:
-                algo.reverse();
-                algo.setAlgoParam(1, 80);
-                algo.submitToMarket();
-                break;
-
-            case 3:
-                algo.setAlgoParam(1, 90);
-                algo.setAlgoParam(2, 15);
-                algo.performCalc();
-                algo.submitToMarket();
-                break;
-
-            default:
-                algo.cancelTrades();
-                break;
+        if (procedure != null)
+        {
+            for (Instruction instr : procedure.getInstructions()) {
+                execute(instr);
+            }
+        }
+        else {
+            algo.cancelTrades();
         }
 
         algo.doAlgo();
+    }
+
+    private void execute(Instruction instr) {
+        try {
+            if (instr.length() == 1) {
+                Method method = algo.getClass().getMethod(instr.getMethodName());
+                method.invoke(algo);
+            }
+            else if (instr.length() == 3) {
+                Method method = algo.getClass().getMethod(instr.getMethodName(), int.class, int.class);
+                method.invoke(algo, Integer.valueOf(instr.getArgument(1)), Integer.valueOf(instr.getArgument(2)));
+            }
+        }
+        catch (NoSuchMethodException e) {
+            System.err.println("No method of name " + instr.getMethodName() + " exists in the Algo library");
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            System.err.println("Invocation target exception.");
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            System.err.println("Illegal access.");
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/pp/exercise/trade/model/Instruction.java
+++ src/main/java/pp/exercise/trade/model/Instruction.java
@@ -1,6 +1,5 @@
 package pp.exercise.trade.model;
 
-
 import java.util.List;
 
 public class Instruction {
@@ -28,5 +27,17 @@ public class Instruction {
 
     public void setParts(List<String> parts) {
         this.parts = parts;
+    }
+
+    public String getMethodName() {
+        return parts.get(0);
+    }
+
+    public String getArgument(Integer index) {
+        return parts.get(index);
+    }
+
+    public Integer length() {
+        return parts.size();
     }
 }
